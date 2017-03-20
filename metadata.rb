db2bb66c811fc5d2b5bb6cd0eb04726fe4a74624
Joe Gardiner <joe@grdnr.io>
2017-03-20 14:23:28 +0000

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'emea_demo_remediation'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures emea_demo_remediation'
+long_description 'Installs/Configures emea_demo_remediation'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/emea_demo_remediation/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/emea_demo_remediation' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: emea_demo_remediation
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
