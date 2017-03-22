f808a4aef174c2baf061e1c547bf6948f593f5a3
Joe Gardiner <joe@grdnr.io>
2017-03-22 16:07:06 +0000

    Basic info and deps added

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,11 @@
 name 'emea_demo_remediation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Joe Gardiner'
+maintainer_email 'joe@chef.io'
 license 'all_rights'
-description 'Installs/Configures emea_demo_remediation'
-long_description 'Installs/Configures emea_demo_remediation'
+description 'Wrapper cookbook for SA demo remediation'
+long_description 'Provides a wrapper cookbook to be used in the EMEA SA Compliance demo. This cookbook remediates Windows and Linux servers by inheriting the windows and OS hardening cookbooks.'
 version '0.1.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/emea_demo_remediation/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/emea_demo_remediation' if respond_to?(:source_url)
+# hardening cookbooks
+depends 'os-hardening'
+depends 'windows-hardening'
