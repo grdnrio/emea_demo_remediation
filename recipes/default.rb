7d6c6a06965576d050982a3e7d086f95686ac583
Joe Gardiner <joe@grdnr.io>
2017-03-22 16:07:16 +0000

    Includes with platform wraps

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+case node['os']
+when 'linux'
+  include_recipe 'os-hardening::default'
+when 'windows'
+  include_recipe 'windows-hardening::default'
+else
+  raise "Unsupported OS #{node['os']}"
+end
